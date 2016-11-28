6dc4e2f00b22d45f6ed30e0797d3bce8b6156ee3
Thomas Cocozzello <tjcocozz@us.ibm.com>
2016-11-28 16:42:51 -0600

    update meta

--- login.php
+++ login.php
@@ -12,6 +12,9 @@
 
    <head>
       <title>Thanks Pearl</title>
+      <meta charset="UTF-8">
+      <meta name="robots" content="NOODP">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
       <link href = "css/bootstrap.min.css" rel = "stylesheet">
       <link rel="stylesheet" type="text/css" href="css/styles.css">
       <link rel="stylesheet" type="text/css" href="css/header.css">
